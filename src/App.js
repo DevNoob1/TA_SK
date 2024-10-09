04fe5d86b7b2eacc10dd49c824888d529daacbcb
Ayush Kumar Rai <66361024+DevNoob1@users.noreply.github.com>
2024-10-09 12:13:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import './App.css';
-// import Sidebar from './components/Sidebar.js';
-import Navbar from './components/Navbar.js';
 import Header from './components/Header.js';
 import Last from './components/Last.js';
 import Profile from './components/Profile.js';
-// import MainBody from './components/MainBody.js';
 
 function App() {
   return (
@@ -13,10 +10,6 @@ function App() {
       <Header />
       <Profile />
       <Last />
-      {/* 
-        <Sidebar />
-        <MainBody />
-      */}
     </div>
   );
 }
