08bcadbaf0268cf9059e3d284c2eadbf9c043c5f
Ayush Kumar Rai <66361024+DevNoob1@users.noreply.github.com>
2024-10-09 12:16:54 +0530

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,19 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import '../styles/Profile.css'; // Import the CSS file
-import womanPic from '../assets/woman.jpg'; // Import the image
+import '../styles/Profile.css';
+import womanPic from '../assets/woman.jpg';
 
 const Profile = () => {
     const [scrollY, setScrollY] = useState(0);
 
-    const handleScroll = () => {
-        setScrollY(window.scrollY - 500);
-    };
-
     useEffect(() => {
+        const handleScroll = () => setScrollY(window.scrollY - 500);
         window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
+        return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
     return (
@@ -25,24 +20,18 @@ const Profile = () => {
                 style={{
                     bottom: `${Math.max(scrollY / 2, 200)}px`,
                     left: '25%',
-                    transform: `scale(${1 + scrollY / 500})`, // Scaling as you scroll
+                    transform: `scale(${1 + scrollY / 500})`,
                 }}
             />
-            <div
-                className={`text-container ${scrollY > 300 ? 'scrolled' : ''}`} // Applying scrolled class based on scroll position
-            >
+            <div className={`text-container ${scrollY > 300 ? 'scrolled' : ''}`}>
                 <p>LOREM INTO IPSUM</p>
                 <p>A LOREM in the IPSUM</p>
             </div>
-            <div
-                className={`section mango-section ${scrollY > 400 ? 'visible' : ''}`} // Add 'visible' class when scrollY > 600
-            >
+            <div className={`section mango-section ${scrollY > 400 ? 'visible' : ''}`}>
                 <h1 style={{ marginTop: '3px' }}>Lorem <br />In <br />Ipsum</h1>
-                <div style={{ height: '9    0px', width: '1px', background: 'white' }}></div>
+                <div style={{ height: '90px', width: '1px', background: 'white' }}></div>
                 <p style={{ marginTop: '2px' }}>Lorem lorem <br />Ipsum Ipsum <br />Lorem lorem <br />Ipsum Ipsum</p>
             </div>
-
-
         </div>
     );
 };
